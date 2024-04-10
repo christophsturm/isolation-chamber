92f3ca079db661f14a2a0767c4f54c0a4cfcd6cb
Christoph Sturm <me@christophsturm.com>
2024-04-10 21:11:40 +0200

    chore: bump amper version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
 }
 
 plugins {
-  id("org.jetbrains.amper.settings.plugin").version("0.2.0")
+  id("org.jetbrains.amper.settings.plugin").version("0.2.2")
 }
 
 //includeBuild("../restaurant")
