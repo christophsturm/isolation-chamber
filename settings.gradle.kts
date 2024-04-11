587cc39cba3905d925e9ad9a4edb727c4bafbafb
Christoph Sturm <me@christophsturm.com>
2024-04-11 17:49:19 +0200

    document what amper deps come from where

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,32 @@
 pluginManagement {
   repositories {
     mavenCentral()
-    google()
+    google {
+      content {
+        includeGroupAndSubgroups("com.android")
+        includeGroupAndSubgroups("com.google.testing.platform")
+        includeGroupAndSubgroups("androidx.databinding")
+      }
+    }
     gradlePluginPortal()
-    maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
-    maven("https://www.jetbrains.com/intellij-repository/releases")
-    maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
+    maven("https://maven.pkg.jetbrains.space/public/p/amper/amper") {
+      content {
+        includeGroupAndSubgroups("org.jetbrains.amper")
+      }
+    }
+    maven("https://www.jetbrains.com/intellij-repository/releases") {
+      content {
+        includeGroup("com.jetbrains.intellij.platform")
+      }
+    }
+    maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies") {
+      content {
+        includeModule("com.jetbrains", "jbr-api")
+        includeModule("org.sqlite", "native")
+        includeGroup("com.github.weisj")
+        includeGroupAndSubgroups("org.jetbrains.intellij.deps")
+      }
+    }
   }
 }
 
